62e341344cb003046e73800ecc511ee62c451905
DMGDy <dylangarza1909@gmail.com>
2025-02-03 04:25:17 -0500

    [status] battery has dedicated tooltip text when hovered and handling case for remaining time left when plugged in charging

--- src/status.rs
+++ src/status.rs
@@ -9,6 +9,7 @@ use time::{OffsetDateTime,format_description};
 const BATTERY_PERCENTAGE: &str = "/sys/class/power_supply/BAT1/capacity";
 const BATTERY_STATUS: &str = "/sys/class/power_supply/BAT1/status";
 const BATTERY_CHARGE: &str = "/sys/class/power_supply/BAT1/charge_now";
+const BATTERY_CHARGE_FULL: &str = "/sys/class/power_supply/BAT1/charge_full";
 const BATTERY_CURRENT: &str = "/sys/class/power_supply/BAT1/current_now";
 
 pub struct DateTime {
@@ -20,14 +21,15 @@ pub struct DateTime {
 enum BatteryStatus {
     Discharging,
     Charging,
-    Not_charging,
+    NotCharging,
     Error
 }
 
 pub struct Battery {
-    pub capacity: u32,
-    pub icon: String,
-    pub remaining: String,
+    pub capacity: u32, //percentage left on battery
+    pub icon: String, //return path to image for icon
+    pub tooltip_text: String, // formatted with remaining
+    remaining: String, 
 
     status: BatteryStatus
 }
@@ -38,6 +40,7 @@ impl Default for Battery {
             capacity: 0,
             icon: String::new(),
             remaining: String::new(),
+            tooltip_text: String::new(),
             status: BatteryStatus::Error
         }
     }
@@ -46,115 +49,164 @@ impl Default for Battery {
 pub struct Memory {
 }
 
-fn get_battery() -> u32 {
+fn get_battery(b: &mut Battery) {
     let mut file = File::open(BATTERY_PERCENTAGE).unwrap();
     let mut buff = String::with_capacity(3);
 
     let _ = file.read_to_string(&mut buff);
 
-    buff.trim_end().parse::<u32>().unwrap()
+    b.capacity = buff.trim_end().parse::<u32>().unwrap();
 }
 
-fn get_status() -> BatteryStatus {
+fn get_status(b: &mut Battery) {
     let mut file = File::open(BATTERY_STATUS).unwrap();
     let mut buff  = String::with_capacity(16);
 
     let _ = file.read_to_string(&mut buff);
 
-    match buff.trim_end() {
+    b.status = match buff.trim_end() {
         "Charging" => BatteryStatus::Charging,
         "Discharging" => BatteryStatus::Discharging,
-        "Not charging"=> BatteryStatus::Not_charging,
+        "Not charging"=> BatteryStatus::NotCharging,
         _ => BatteryStatus::Error
-    }
+    };
         
 }
 
-fn get_remaining(b: &Battery)  -> String{
-    // read current charge
-    if b.status == BatteryStatus::Not_charging {
-        return "Plugged In, not Charging".to_string();
-    }
-    let mut file = File::open(BATTERY_CHARGE).unwrap();
-    let mut buff = String::with_capacity(32);
+fn get_battery_tooltip_text(b: &mut Battery) {
+    let status_text = match b.status {
+        BatteryStatus::Charging => "Charging",
+        BatteryStatus::Discharging => "Discharging",
+        BatteryStatus::NotCharging=> "Plugged in, Not Charging",
+        _ => "Error getting state"
+    };
 
-    let _ = file.read_to_string(&mut buff);
-
-    let Q = buff.trim_end().parse::<f64>().unwrap();
-
-    buff.clear();
-    // get current_now
-    let mut file2 = File::open(BATTERY_CURRENT).unwrap();
-    let _ = file2.read_to_string(&mut buff);
-
-    let I = buff.trim_end().parse::<f64>().unwrap();
-
-    // time remaining in seconds
-    let t: f64 =  (Q * 3600f64) / I;
-    let t_h: f64 = t / 3600f64;
-    let t_m: f64 = (t % 3600f64)/60f64;
-
-    format!("{t_h} H {t_m} m remainign ").to_string()
-       
+    b.tooltip_text = format!("{status_text}\n{}",b.remaining);
 }
 
-fn get_icon(b: &Battery) -> String {
+// TODO: not considering charging until full time
+fn get_remaining(b: &mut Battery) {
+    // read current charge
+    match &b.status {
+        state @ (BatteryStatus::Charging | BatteryStatus::Discharging)=> {
+            let mut file = File::open(BATTERY_CHARGE).unwrap();
+            let mut buff = String::with_capacity(32);
+            let _ = file.read_to_string(&mut buff);
+            let charge_now = buff.trim_end().parse::<f64>().unwrap();
+
+            buff.clear();
+
+            // get current_now
+            let mut file2 = File::open(BATTERY_CURRENT).unwrap();
+            let _ = file2.read_to_string(&mut buff);
+            let current = buff.trim_end().parse::<f64>().unwrap();
+
+            match state {
+                BatteryStatus::Charging =>{
+                    buff.clear();
+                    // get full charge of battery to find remaining charge
+                    let _ = File::open(BATTERY_CHARGE_FULL)
+                        .unwrap()
+                        .read_to_string(&mut buff);
+                    let charge_remaining = buff.trim_end()
+                        .parse::<f64>()
+                        .unwrap() - charge_now;
+                    let t = (charge_remaining*3600f64)/current;
+                    let t_h = t/3600f64;
+                    let t_m = (t%3600f64)/60f64;
+
+                    if t_h >= 1.0 {
+                        b.remaining = format!("{} hours {} minutes to full"
+                            ,t_h as u32,t_m as u32).to_string();
+                    } else {
+                        b.remaining = format!("{} minutes to full"
+                            ,t_m as u32).to_string();
+                    }
+
+
+                },
+                BatteryStatus::Discharging => {
+                    // time remaining in seconds
+                    let t: f64 =  (charge_now * 3600f64) / current;
+                    let t_h: f64 = t / 3600f64;
+                    let t_m: f64 = (t % 3600f64)/60f64;
+
+                    if t_h >= 1.0 {
+                        b.remaining = format!("{} hours {} minutes remaining"
+                            ,t_h as u32,t_m as u32).to_string();
+                    } else {
+                        b.remaining = format!("{} minutes remaining"
+                            ,t_m as u32).to_string();
+                    }
+                }
+                _ => {()}
+            };
+
+
+        },
+        BatteryStatus::NotCharging => b.remaining = "Fully charged".to_string(),
+        _ => b.remaining = "Error getting state".to_string(),
+
+    };
+}
+
+fn get_icon(b: &mut Battery) {
 
     let capacity = b.capacity;
     let status = &b.status;
 
-    match status {
+    b.icon = match status {
         BatteryStatus::Charging =>{
             match capacity {
-                0..=10=> "󰢟".to_string(),
-                11..=19=>"󰢜".to_string(),
-                20..=29=>"󰂆".to_string(),
-                30..=39=>"󰂇".to_string(),
-                40..=49=>"󰂈".to_string(),
-                50..=59=>"󰢝".to_string(),
-                60..=69=>"󰂉".to_string(),
-                70..=79=>"󰢞".to_string(),
-                80..=89=>"󰂊".to_string(),
-                90..=94=>"󰂋".to_string(),
-                95..=100=>"󰂅".to_string(),
-                _ => "󰂑".to_string()
+                0..=10=> "status/battery-000-charging.svg".to_string(),
+                11..=19=>"status/battery-010-charging.svg".to_string(),
+                20..=29=>"status/battery-020-charging.svg".to_string(),
+                30..=39=>"status/battery-030-charging.svg".to_string(),
+                40..=49=>"status/battery-040-charging.svg".to_string(),
+                50..=59=>"status/battery-050-charging.svg".to_string(),
+                60..=69=>"status/battery-060-charging.svg".to_string(),
+                70..=79=>"status/battery-070-charging.svg".to_string(),
+                80..=89=>"status/battery-080-charging.svg".to_string(),
+                90..=94=>"status/battery-090-charging.svg".to_string(),
+                95..=100=>"status/battery-100-charging.svg".to_string(),
+                _ => "status/battery-missing.svg".to_string()
             }
         },
         
         BatteryStatus::Discharging => {
             match capacity {
-                0..=10=> "󱃍".to_string(),
-                11..=19=>"󰁺".to_string(),
-                20..=29=>"󰁻".to_string(),
-                30..=39=>"󰁼".to_string(),
-                40..=49=>"󰁽".to_string(),
-                50..=59=>"󰁾".to_string(),
-                60..=69=>"󰁿".to_string(),
-                70..=79=>"󰂀".to_string(),
-                80..=89=>"󰂁".to_string(),
-                90..=94=>"󰂂".to_string(),
-                95..=100=>"󰁹".to_string(),
-                _ => "󰂑".to_string()
+                0..=10=>  "status/battery-000.svg".to_string(),
+                11..=19=> "status/battery-010.svg".to_string(),
+                20..=29=> "status/battery-020.svg".to_string(),
+                30..=39=> "status/battery-030.svg".to_string(),
+                40..=49=> "status/battery-040.svg".to_string(),
+                50..=59=> "status/battery-050.svg".to_string(),
+                60..=69=> "status/battery-060.svg".to_string(),
+                70..=79=> "status/battery-070.svg".to_string(),
+                80..=89=> "status/battery-080.svg".to_string(),
+                90..=94=> "status/battery-090.svg".to_string(),
+                95..=100=>"status/battery-100.svg".to_string(),
+                _ => "status/battery-missing.svg".to_string()
             }
         },
-        BatteryStatus::Not_charging => {
+        BatteryStatus::NotCharging => {
             match capacity {
-                97..=100=> "󱈑".to_string(),
-                _ => "󱃍".to_string()
+                97..=100=> "status/battery-full-charging.svg".to_string(),
+                _ => "status/battery-missing.svg".to_string()
             }
         }
-        _ => "ruhro".to_string()
+        _ => "status/battery-missing.svg".to_string()
     }
 }
 
 pub fn get_battery_info() -> Battery {
     let mut battery = Battery::default();
-    battery.capacity = get_battery();
-    battery.status = get_status();
-    battery.icon = get_icon(&battery);
-
-    battery.remaining = get_remaining(&battery);
-
+    // in this order
+    get_battery(&mut battery);
+    get_status(&mut battery);
+    get_icon(&mut battery);
+    get_remaining(&mut battery);
+    get_battery_tooltip_text(&mut battery);
     battery
 }
 
@@ -179,7 +231,7 @@ pub fn get_datetime() -> DateTime {
     }
 }
 
-// so can be easily printed out
+// so can be easily format out
 impl fmt::Display for DateTime {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f,"{}\n{}",self.date,self.time)
