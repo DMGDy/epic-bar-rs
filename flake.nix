dbf2ed6fd2bb6b45bc181d82608a3dbe3c95d519
DMGDy <dylangarza1909@gmail.com>
2025-02-06 00:59:13 -0500

    [flake] added executable to inputs

--- flake.nix
+++ flake.nix
@@ -1,50 +1,59 @@
 {
-    description = "Custom status bar for Hyprland compositor";
-
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-        rust-overlay = {
-            url = "github:oxalica/rust-overlay";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+  description = "Custom status bar for Hyprland compositor";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+  };
 
-    outputs = { self, nixpkgs, rust-overlay}: 
-        let
-        system = "x86_64-linux";
-    overlays = [ (import rust-overlay) ];
-    pkgs = import nixpkgs { inherit system overlays; };
+  outputs = { self, nixpkgs, rust-overlay }: 
+    let
+    system = "x86_64-linux";
+  overlays = [ (import rust-overlay)  ];
+  pkgs = import nixpkgs { inherit system overlays;  };
 
-    rustToolchain = pkgs.rust-bin.stable.latest.minimal;
+  rustToolchain = pkgs.rust-bin.stable.latest.minimal;
 
-    rustPlatform = pkgs.makeRustPlatform {
-        cargo = rustToolchain;
-        rustc = rustToolchain;
+  rustPlatform = pkgs.makeRustPlatform {
+    cargo = rustToolchain;
+    rustc = rustToolchain;
+  };
+  version = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package.version;
+  epic-bar = rustPlatform.buildRustPackage {
+    pname = "epic-bar-rs";
+    version = "${version}";
+    src = ./.;
+    cargoLock.lockFile = ./Cargo.lock;
+
+    buildInputs = with pkgs; [
+      dbus
+      glib
+      pango
+      gdk-pixbuf
+      graphene
+      gtk4
+      gtk4-layer-shell
+      librsvg
+      libxml2
+    ];
+
+    nativeBuildInputs = with pkgs.buildPackages; [
+      pkg-config
+      wrapGAppsHook
+    ];
+
+  };
+  in {
+    packages.${system}.default = epic-bar;
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        rustToolchain
+        epic-bar  
+      ] ++ epic-bar.buildInputs;  
+
+      nativeBuildInputs = epic-bar.nativeBuildInputs;
     };
-        version = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package.version;
-    in {
-        packages.${system}.default = rustPlatform.buildRustPackage {
-            pname = "epic-bar-rs";
-            version = "${version}-dirty";
-            src = ./.;
-            cargoLock.lockFile = ./Cargo.lock;
-
-            buildInputs = with pkgs; [
-                dbus
-                glib
-                pango
-                gdk-pixbuf
-                graphene
-                gtk4
-                gtk4-layer-shell
-                librsvg
-                libxml2
-            ];
-
-            nativeBuildInputs = with pkgs.buildPackages; [
-                pkg-config
-                wrapGAppsHook
-            ];
-        };
-    };
+  };
 }
