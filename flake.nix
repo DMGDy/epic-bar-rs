c0a71839242b9955723ade8125d1b873a9cb0872
DMGDy <dylangarza1909@gmail.com>
2025-02-06 00:11:00 -0500

    [flake] created for dev shell and running normally

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+    description = "Custom status bar for Hyprland compositor";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+        rust-overlay = {
+            url = "github:oxalica/rust-overlay";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+        epic-bar-rs.url = "github:DMGDy/epic-bar-rs";
+    };
+
+    outputs = { self, nixpkgs, rust-overlay, epic-bar-rs}: 
+        let
+        system = "x86_64-linux";
+    overlays = [ (import rust-overlay) ];
+    pkgs = import nixpkgs { inherit system overlays; };
+
+    rustToolchain = pkgs.rust-bin.stable.latest.minimal;
+
+    rustPlatform = pkgs.makeRustPlatform {
+        cargo = rustToolchain;
+        rustc = rustToolchain;
+    };
+        version = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package.version;
+    in {
+        packages.${system}.default = rustPlatform.buildRustPackage {
+            pname = "epic-bar-rs";
+            version = "${version}-dirty";
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+
+            buildInputs = with pkgs; [
+                dbus
+                glib
+                pango
+                gdk-pixbuf
+                graphene
+                gtk4
+                gtk4-layer-shell
+                librsvg
+                libxml2
+                epic-bar-rs.defaultPackage.${system}
+            ];
+
+            nativeBuildInputs = with pkgs.buildPackages; [
+                pkg-config
+                wrapGAppsHook
+            ];
+        };
+    };
+}
